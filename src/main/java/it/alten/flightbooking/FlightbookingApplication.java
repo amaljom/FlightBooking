8fd274acc3cb23a8e7a87a0655cfebb4a9f95a9b
amaljom <103516697+amaljom@users.noreply.github.com>
2023-03-10 09:54:31 +0100

    initial commit

---
+++ src/main/java/it/alten/flightbooking/FlightbookingApplication.java
@@ -0,0 +1,15 @@
+package it.alten.flightbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightbookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightbookingApplication.class, args);
+	}
+
+
+	
+}
