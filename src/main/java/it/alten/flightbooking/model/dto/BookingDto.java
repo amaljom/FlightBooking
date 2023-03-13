0188ec5189817adccb436f5c616d99675ff3ea6a
amaljom <103516697+amaljom@users.noreply.github.com>
2023-03-13 12:38:15 +0100

    added lombok annotation to dto

--- src/main/java/it/alten/flightbooking/model/dto/BookingDto.java
+++ src/main/java/it/alten/flightbooking/model/dto/BookingDto.java
@@ -1,11 +1,20 @@
 package it.alten.flightbooking.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.Column;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookingDto {
 
     private Long id;
