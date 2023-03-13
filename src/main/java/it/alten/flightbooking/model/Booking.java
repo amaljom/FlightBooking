7b701174d6db0998299851b0fd48acf5505e413f
amaljom <103516697+amaljom@users.noreply.github.com>
2023-03-13 12:39:19 +0100

    updates on booking and and bookig repo

--- src/main/java/it/alten/flightbooking/model/Booking.java
+++ src/main/java/it/alten/flightbooking/model/Booking.java
@@ -4,8 +4,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.lang.Nullable;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -14,30 +16,40 @@ import java.time.LocalDateTime;
 @Entity
 @NoArgsConstructor
 @Table(name="booking")
-public class Booking {
+public class Booking implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column
+
     private LocalDate flightDate;
     @Column
     @DateTimeFormat(pattern = "HH:mm:ss")
+
     private LocalDateTime arrivalTime;
+
     @Column
     @DateTimeFormat(pattern = "HH:mm:ss")
     private LocalDateTime departureTime;
 
     @Column
+
     private String departureAirport;
 
     @Column
+
     private String arrivalAirport;
     @Column
+
     private int bookedSeats;
 
     @Column
     private String passengerName;
+
+//    @ManyToOne
+//    @JoinColumn(name = "flight_id")
+//    private Flight flight;
 
 }
